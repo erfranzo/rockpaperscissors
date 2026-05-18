872f0c6c4b7ec8b5350963113b94d2635c845a61
Eric <51839013+erfranzo@users.noreply.github.com>
2026-05-18 10:14:44 -0700

    added playRound function in for loop for three rounds

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+let humanScore = 0;
+let computerScore = 0;
 
-// Produces a random number between 0 and 2 and returns rock paper or scissors.
+// Produces a random number between 0 and 2 and returns rock paper or scissors
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3);
     if (choice === 0) {
@@ -20,5 +22,28 @@ function getPlayerChoice() {
     return choice.toLowerCase();
 }
 
-console.log(getPlayerChoice());
-console.log(getComputerChoice());
+// Plays a single round of rock paper scissors and updates the score
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        console.log("You just tied");
+    }
+   else if (humanChoice === 'scissors' && computerChoice === 'paper' || humanChoice === 'rock' && computerChoice === 'scissors' || humanChoice === 'paper' && computerChoice === 'rock') {
+    console.log('You win!');
+    humanScore++;
+    }
+    else {
+        console.log('You lose');
+        computerScore++;
+    }
+}
+
+// Plays 3 rounds of rock paper scissors and keeps score
+for (let i = 0; i < 3; i++) {
+    const computerSelection = getComputerChoice();
+const playerSelection = getPlayerChoice();
+
+    playRound(playerSelection, computerSelection);
+    
+    console.log('Your score is '+ humanScore);
+    console.log('The computer score is ' + computerScore);
+}
