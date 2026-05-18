4fdc47203379eee2cf31480b40b0a52cc4a0382c
Eric <51839013+erfranzo@users.noreply.github.com>
2026-05-18 09:27:22 -0700

    added playerchoice function

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+
+// Produces a random number between 0 and 2 and returns rock paper or scissors.
 function getComputerChoice() {
-    let choice =Math.floor(Math.random() * 3);
+    let choice = Math.floor(Math.random() * 3);
     if (choice === 0) {
         return 'rock';
     }
@@ -12,4 +14,11 @@ function getComputerChoice() {
 
 }
 
+// Allows player to input choice
+function getPlayerChoice() {
+    let choice = prompt("Choose Rock, Paper, or Scissors");
+    return choice.toLowerCase();
+}
+
+console.log(getPlayerChoice());
 console.log(getComputerChoice());
